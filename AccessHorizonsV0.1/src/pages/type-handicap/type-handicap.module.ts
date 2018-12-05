257d5dd9a67902b430670a549d843f66b0402f35
krisfresno <41947126+krisfresno@users.noreply.github.com>
2018-12-05 11:37:42 +0100

    Création page TypeHandicap

---
+++ AccessHorizonsV0.1/src/pages/type-handicap/type-handicap.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { TypeHandicapPage } from './type-handicap';
+
+@NgModule({
+  declarations: [
+    TypeHandicapPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(TypeHandicapPage),
+  ],
+})
+export class TypeHandicapPageModule {}
