120cd81e2c3e03c33f0d371208416f01c01d874c
Raphaël Chalicarne <raphael.chalicarne@outlook.com>
2018-12-06 16:40:22 +0100

    Annulation modif liens
    Annulation de la modification des liens

--- AccessHorizonsV0.1/src/app/app.module.ts
+++ AccessHorizonsV0.1/src/app/app.module.ts
@@ -7,9 +7,13 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 <<<<<<< HEAD
+<<<<<<< HEAD
 import { userProfile } from '../user-profile/user-profile';
 =======
 >>>>>>> parent of 6c0df32... Ajout de la page UserProfilePage
+=======
+//import { userProfile } from '..pages/user-profile/user-profile';
+>>>>>>> parent of 2de57b7... Liens pour accéder à userProfilePage
 
 @NgModule({
   declarations: [
--- AccessHorizonsV0.1/src/pages/home/home.ts
+++ AccessHorizonsV0.1/src/pages/home/home.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 <<<<<<< HEAD
+<<<<<<< HEAD
 import { UserProfilePage } from '../user-profile/user-profile';
 =======
 >>>>>>> parent of 6c0df32... Ajout de la page UserProfilePage
+=======
+import { UserProfilePage } from '..pages/user-profile/user-profile';
+>>>>>>> parent of 2de57b7... Liens pour accéder à userProfilePage
 
 @Component({
   selector: 'page-home',
