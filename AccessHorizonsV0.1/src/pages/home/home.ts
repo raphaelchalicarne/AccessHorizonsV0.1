56d1cdbaffde0bce5b83d024263aae192cf3f52f
Raphaël Chalicarne <raphael.chalicarne@outlook.com>
2018-12-06 17:24:28 +0100

    Page UserProfile
    Ajout de la page UserProfilePage et de la méthode pour accéder à cette page depuis la page d'accueil

--- AccessHorizonsV0.1/src/pages/home/home.ts
+++ AccessHorizonsV0.1/src/pages/home/home.ts
@@ -7,8 +7,11 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(public navCtrl: NavController) {}
+  
+  goToUserProfilePage() {
+      this.navCtrl.push(UserProfilePage);
   }
 
 }
+class UserProfilePage { }
