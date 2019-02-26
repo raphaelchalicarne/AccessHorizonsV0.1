663cdf279c207bb5a76bfae3d7b8fac71c44ba0d
krisfresno <41947126+krisfresno@users.noreply.github.com>
2019-02-26 16:22:03 +0100

    Modification jaccede.ts

--- AccessHorizonsV0.1/src/providers/jaccede/jaccede.ts
+++ AccessHorizonsV0.1/src/providers/jaccede/jaccede.ts
@@ -1,17 +1,35 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
-/*
-  Generated class for the JaccedeProvider provider.
-
-  See https://angular.io/guide/dependency-injection for more info on providers
-  and Angular DI.
-*/
 @Injectable()
 export class JaccedeProvider {
+  apiKey: string = '93e6cdc203eeca0079b935f2370dee27d9840c34f1b064a9b71cd7292bde6a9b';
 
   constructor(public http: HttpClient) {
-    console.log('Hello JaccedeProvider Provider');
+  }
+  getPlaces(longitud, latitud){
+    let myUrl = 'https://apidev.jaccede.com/v4/places/search?lng='+longitud+'&lat='+latitud+'&per_page=50&lang=fr&api_key='+this.apiKey+'';
+    return this.http.get(myUrl)
+  }
+  getAutocomplete(city){
+    let limite = '2';
+    let autoUrl = 'https://photon.komoot.de/api/?q='+city+'&limit='+limite+'';
+    return this.http.get(autoUrl)
+  }
+  getDetails(googleID){
+  	return this.http.get('https://apidev.jaccede.com/v4/places/'+googleID+'?lang=fr&api_key='+this.apiKey+'');
+  }
+  getComments(googleID){
+    let myUrl = 'https://apidev.jaccede.com/v4/places/'+googleID+'/comments?api_key='+this.apiKey+'';
+    return this.http.get(myUrl)  
   }
 
+
+
+  /*getFilters(){
+  	//return this.http.get('/filters')
+  	return this.http.get('https://apidev.jaccede.com/v4/accessibility_filters?lang=fr&api_key=93e6cdc203eeca0079b935f2370dee27d9840c34f1b064a9b71cd7292bde6a9b')
+  } */
+  
 }
+
