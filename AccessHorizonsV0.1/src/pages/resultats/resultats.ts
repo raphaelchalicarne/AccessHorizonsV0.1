f43dc20ecc5edc63fb08a85eb6beef01a29c4050
krisfresno <41947126+krisfresno@users.noreply.github.com>
2019-02-26 18:35:44 +0100

    Changement Page resultats

--- AccessHorizonsV0.1/src/pages/resultats/resultats.ts
+++ AccessHorizonsV0.1/src/pages/resultats/resultats.ts
@@ -1,12 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
-/**
- * Generated class for the ResultatsPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import { JaccedeProvider } from '../../providers/jaccede/jaccede';
+import { PlaceResultPage } from '../place-result/place-result';
 
 @IonicPage()
 @Component({
@@ -14,12 +9,46 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'resultats.html',
 })
 export class ResultatsPage {
+  places: any[] = [];
+  longitud: number; 
+  latitud:  number;
+  adresse: string = '';
+  resultat: any[] = [];
+  filtrage:any = [];
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  ngOnInit() {
+      this.adresse = this.navParams.get('adresse');
+      this.longitud = this.navParams.get('longitud');
+      this.latitud = this.navParams.get('latitud');
+      this.filtrage = this.navParams.get('filtrage');
   }
 
+  constructor(public navCtrl: NavController, public navParams: NavParams, public userService: JaccedeProvider) {
+  }
   ionViewDidLoad() {
-    console.log('ionViewDidLoad ResultatsPage');
+    this.userService.getPlaces(this.longitud, this.latitud)
+    .subscribe(
+      (data) => { 
+        this.places = data['items'];
+        if (this.filtrage.length == 0) {
+          this.resultat = this.places;
+        }
+        if (this.filtrage.length != 0) {
+          var i;
+          for (i in this.places){
+            if (this.filtrage.includes(this.places[i].category.name)) {
+              this.resultat.push(this.places[i]);
+            }
+          }
+        }
+      (error) =>{
+        console.error(error);
+      }
+    }
+  )
   }
-
+  goToPlace(name: string, adresse: string, googleID: string){
+    this.navCtrl.push(PlaceResultPage, {name:name, adresse:adresse, googleID:googleID});
+  }
+  
 }
