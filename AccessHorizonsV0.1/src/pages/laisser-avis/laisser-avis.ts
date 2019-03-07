59c3595feedc40b58cb8264554224cbb9bfc7b06
krisfresno <41947126+krisfresno@users.noreply.github.com>
2019-03-07 21:34:43 +0100

    actualiser laisser avis

--- AccessHorizonsV0.1/src/pages/laisser-avis/laisser-avis.ts
+++ AccessHorizonsV0.1/src/pages/laisser-avis/laisser-avis.ts
@@ -7,6 +7,11 @@ import { IonicPage, NavController, NavParams, ViewController } from 'ionic-angul
   templateUrl: 'laisser-avis.html',
 })
 export class LaisserAvisPage {
+  buttons = [1,2,3,4,5];
+  note_globale: number = 2;
+  note_finale: number;
+  nombre: number = 1;
+  isOutline:any[] = [true,true,true,true,true];
 
   constructor(public navCtrl: NavController, public navParams: NavParams, public viewCtrl: ViewController) {
   }
@@ -18,9 +23,22 @@ export class LaisserAvisPage {
   	let message = "Modal closed";
   	this.viewCtrl.dismiss({data : message});
   }
+  recalculerNote(element: number){
+    for (var i =1; i < 6; ++i){
+      if (i != element) {
+        this.isOutline[i-1] = true;
+      }
+      else{
+        this.isOutline[i-1] = false;
+      }
+    }
+    console.log(this.note_globale);
+    this.note_finale = (this.nombre/(this.nombre+1))*this.note_globale + element/(this.nombre+1);
+    console.log(this.note_finale);
+  }
+  
   /*lors qu'on retourne à la page principale de resultats, les infos
   introduites par l'utilisateur sont envoyés à la base de donnés*/
-
   updateFirebase(){
     //ICI code pour envoyer donnés à la base de donnés
     this.viewCtrl.dismiss();
