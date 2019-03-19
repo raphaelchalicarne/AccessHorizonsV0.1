0f50c16b908c97496136c8b7421d31699222541c
Raphaël Chalicarne <raphael.chalicarne@outlook.com>
2019-03-19 17:42:43 +0100

    Changement 1e page connexion
    Si une personne se connecte pour la 2e fois ou plus, il n'a plus à dire son type de handicap. Il doit pouvoir modifier ses préférences dans "Paramètres"

--- AccessHorizonsV0.1/src/pages/auth/auth.ts
+++ AccessHorizonsV0.1/src/pages/auth/auth.ts
@@ -6,13 +6,14 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import * as firebase from 'firebase';
 
 import { HomePage } from '../home/home';
+import { MenuServicesPage } from '../menu-services/menu-services';
 
 @Component({
   selector: 'page-auth',
   templateUrl: './auth.html'
 })
 export class AuthPage implements OnInit {
-    
+
   mode: string;
   authForm: FormGroup;
   errorMessage: string;
@@ -27,18 +28,18 @@ export class AuthPage implements OnInit {
       this.mode = this.navParams.get('mode');
       this.initForm();
   }
-    
+
   onToggleMenu() {
     this.menuCtrl.open();
   }
-    
+
   initForm() {
       this.authForm = this.formBuilder.group({
           email: ['',[Validators.required, Validators.email]],
           password: ['', Validators.required]
       });
   }
-    
+
   onSubmitForm() {
       const email = this.authForm.get('email').value;
       const password = this.authForm.get('password').value;
@@ -54,7 +55,7 @@ export class AuthPage implements OnInit {
       } else if (this.mode === 'connect') {
           this.authService.signInUser(email,password).then(
           () => {
-              this.navCtrl.setRoot(HomePage);
+              this.navCtrl.setRoot(MenuServicesPage);
           },
           (error) => {
               this.errorMessage = error;
