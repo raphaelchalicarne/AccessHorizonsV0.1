ed5d25a8472fb3559f07e8397ced18358f833638
krisfresno <41947126+krisfresno@users.noreply.github.com>
2019-03-22 23:04:18 +0100

    modification totale de RecherchePage
    Changement de la page Recherche pour montrer le Map de Leaflet

--- AccessHorizonsV0.1/src/pages/recherche/recherche.ts
+++ AccessHorizonsV0.1/src/pages/recherche/recherche.ts
@@ -1,17 +1,20 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import { ResultatsPage } from '../resultats/resultats';
-import { JaccedeProvider } from '../../providers/jaccede/jaccede';
+import { Component, ViewChild, ElementRef} from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+//import { ResultatsPage } from '../resultats/resultats';
+//import { JaccedeProvider } from '../../providers/jaccede/jaccede';
+import leaflet from 'leaflet';
 
-@IonicPage()
+import { enableProdMode } from '@angular/core';
+enableProdMode();
+
+//@IonicPage()
 @Component({
   selector: 'page-recherche',
   templateUrl: 'recherche.html',
 })
+/*
 export class RecherchePage {
   adresse: string = '';
-  //longitud: number; 
-  //latitud:  number;
   resultat: any[] = [];
   filtrage: any = [];
 
@@ -29,7 +32,7 @@ export class RecherchePage {
     .subscribe(
       (data) => {
         this.resultat = data['features'];
-        //console.log('data', data);
+        console.log('data', data);
       },
       (error) =>{
         console.error(error);
@@ -39,5 +42,40 @@ export class RecherchePage {
   goToPlaceList(longitud: number, latitud: number, adresse: string){ //Passer à la page de liste de places 
     var filtrage = this.filtrage;
     this.navCtrl.push(ResultatsPage, {longitud :longitud, latitud: latitud, adresse:adresse, filtrage:filtrage});
+  }
+}*/
+
+export class RecherchePage {
+  @ViewChild("mapa") mapContainer: ElementRef;
+  center: any = [45.75, 4.83];
+  message: string = 'Casa';
+  lat: number = 45.75334;
+  long: number = 4.842;
+  longitud: number;
+  object: any[] = [];
+  marker: any;
+  adresse: any;
+  filtrage: any[] = [];
+
+  /*ngOnInit() {
+    this.filtrage = this.navParams.get('filtrage');
+  }*/
+  constructor(public navCtrl: NavController, public navParams: NavParams){}
+
+  ionViewDidLoad(){
+    this.loadmap();
+    console.log('Load');
+  }
+  loadmap(){
+    var mapa;
+    mapa = leaflet.map("mapa",{
+      center: this.center,
+      zoom: 13
+    });
+    leaflet.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1Ijoia3JpczExc2lyayIsImEiOiJjanRrMDh5NGEwMm1lM3ltc21kMDRtd3h3In0.SrKlBOp57MHXmgwFT6wSPw',{
+      maxZoom: 19, //zoom possible de faire
+      attribution: 'Map data &copy; ',
+      id: 'mapbox.streets'
+    }).addTo(mapa);
   }
 }
